14ff00bf510dc6d4efc877f48ecaf32b0f874aa3
Fazli Yazid <fazliyazid1702@gmail.com>
2023-04-11 17:29:21 +0800

    edit and save user changes on ejss/xml file
    ISSUES:
    having some problems with special characters/emoji,
    hence only works on some files (e.g can you escape)
    
    only local xml file edited -- user still downloads old version

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -35,6 +35,7 @@ export default class Editor extends Component {
     // retrieve doc
     const { variables, functions } = this.state;
     var doc = this.state.doc;
+    var xDoc = this.readXML();
 
     const variableNames = Object.keys(variables);
     const functionNames = Object.keys(functions);
@@ -42,16 +43,54 @@ export default class Editor extends Component {
     for (var i = 0; i < variableNames.length; i++) {
       const varName = variableNames[i];
       const value = variables[varName];
+      // console.log(varName, value)
       if (!_.isUndefined(value)) {
         // search and replace
-        console.log(
-          `${EDITABLE_VARIABLES_REGEX.replace("[a-zA-Z0-9]+", varName)}`
-        );
+        // xhtml / html
+        // console.log(
+        //   `${EDITABLE_VARIABLES_REGEX.replace("[a-zA-Z0-9]+", varName)}`
+        // );
         var re = new RegExp(
           `${EDITABLE_VARIABLES_REGEX.replace("[a-zA-Z0-9]+", varName)}`
         ); // regex to search for variable name to be replaced in xhtml
         var res = doc.replace(re, `$1${value}$3$4`);
         doc = res;
+
+        // ejss / xml file
+        var variableTags = xDoc.getElementsByTagName("Variable");
+
+        for (let j = 0; j < variableTags.length; j++) {
+          let variableTag = variableTags[j];
+          let variableName = variableTag.firstElementChild.childNodes[0].nodeValue;
+          if (variableName == varName) {
+            let x = variableTag.children;
+            for (let k = 0; k < x.length; k++) {
+              let child = x[k];
+              if (child.nodeName == "Value") {
+                // update value
+                // console.log(`before: ${xDoc.getElementsByTagName("Variable")[j].children[k].childNodes[0].nodeValue}`);
+                // console.log(xDoc.getElementsByTagName("Variable")[j].children[k].childNodes[0].nodeValue);
+                // console.log(xDoc.getElementsByTagName("Variable")[j].children[k].childNodes[0]);
+                xDoc.getElementsByTagName("Variable")[j].children[k].childNodes[0].nodeValue = value;
+                // console.log(`after: ${xDoc.getElementsByTagName("Variable")[j].children[k].childNodes[0].nodeValue}`);
+              }
+            }
+          }
+          
+
+          // Check name
+          // let nameTags = variableTag.getElementsByTagName("Name");
+          // if (nameTags.length < 1) continue;
+          // let nameTag = nameTags[0];
+          // if (nameTag.textContent == varName) {
+          //   let valueTags = variableTag.getElementsByTagName("Value");
+          //   if (valueTags.length < 1) continue;
+          //   let valueTag = valueTags[0];
+          //   console.log(`${nameTag.textContent} = ${valueTag.textContent}`);
+          //   console.log(variableTag);
+          //   xDoc.getElementsByTagName("Variable")[j].childNodes
+          // }
+        }
       }
     }
 
@@ -69,14 +108,56 @@ export default class Editor extends Component {
             .replace(/\\n/g, "\n ")
             .slice(1, -1)
             .replace(/\\"/g, '"');
+
         var res = doc.replace(re, `function $1$2${formattedString}\n$4`);
         doc = res;
+
+        // ejss / xml file
+        var functionTags = xDoc.getElementsByTagName("Osejs.Model.Library.Page");
+        // console.log(functionTags);
+        for (let j = 0; j < functionTags.length; j++) {
+          let functionTag = functionTags[j];
+          let functionName = functionTag.getElementsByTagName("Name")[0].textContent;
+          if (functionName == "EditableFunction") {
+            let content = functionTag.lastElementChild;
+            let k = functionTag.children.length;
+
+            let code = content.lastElementChild;
+            let x = content.children.length;
+
+            let functionText = xDoc.getElementsByTagName("Osejs.Model.Library.Page")[j].children[k-1].children[x-1].textContent;
+            
+            // add function name and curly bracket at start and end of function
+            let formatFunction = "";
+            for (let y = 0; y < functionText.length; y++) {
+              if (functionText[y] == "{") {
+                formatFunction += functionText[y] + "\n";
+                break;
+              }
+              formatFunction += functionText[y];
+            }
+            formatFunction += value + "}";
+
+            // console.log(functionText);
+            // console.log(formatFunction);
+            
+            // console.log("before");
+            // console.log(xDoc.getElementsByTagName("Osejs.Model.Library.Page")[j].children[k-1].children[x-1].textContent);
+
+            xDoc.getElementsByTagName("Osejs.Model.Library.Page")[j].children[k-1].children[x-1].textContent = formatFunction;
+
+            // console.log("after");
+            // console.log(xDoc.getElementsByTagName("Osejs.Model.Library.Page")[j].children[k-1].children[x-1].textContent);
+          }
+        }
       }
     }
+    // console.log(xDoc);
 
     this.setState({
       isSaved: true,
       doc: doc,
+      xDoc: xDoc,
     });
   };
 
@@ -151,16 +232,21 @@ export default class Editor extends Component {
     }
   }
 
-  findCommentInXML = (variable) => {
+  readXML = () => {
     var parser = new DOMParser();
     var xDoc = parser.parseFromString(
       iconv.decode(Buffer.from(this.state.ejssFile.slice(3)), "utf16"),
       "text/xml"
     );
+    return xDoc;
+  }
 
+  findCommentInXML = (variable) => {
+    var xDoc = this.readXML();
     var x = xDoc.getElementsByTagName("Variable");
 
     for (var i = 0; i < x.length; i++) {
+      // console.log(x[i]);
       let variableName = x[i].firstElementChild.childNodes[0].nodeValue;
       if (variableName === variable) {
         let nodeName = x[i].lastElementChild.nodeName;
